c6540b539cf8dcb34319fd97cff08f5c478c1fd1
Pedro <pedro.pelayo003@gmail.com>
2024-04-25 09:06:46 +0200

    Actividad 1 funcionando. Pendiente cambiar el fetch de la api de perros por axios

---
+++ Actividad01/main.js
@@ -0,0 +1,42 @@
+document.addEventListener('DOMContentLoaded', init);
+
+const $ = (selector) => document.querySelector(selector);
+
+const $$ = (selector) => Array.from(document.querySelectorAll(selector))
+
+const URL_PERROS = 'https://dog.ceo/api/breeds/image/random'
+
+const URL_GATOS = 'https://api.thecatapi.com/v1/images/search'
+
+
+function init() {
+    setListeners()
+
+    cargarImagenPerros()
+    cargarImagenGatos()
+}
+
+
+function setListeners() {
+    $('#btn-perros').addEventListener('click', cargarImagenPerros)
+    $('#btn-gatos').addEventListener('click', cargarImagenGatos)
+}
+
+function cargarImagenPerros() {
+    console.log('perros')
+    fetch(URL_PERROS)
+        .then(response => response.json())
+        .then(data => {
+            const url = data.message
+            $('#img-perros').src = url
+        })
+}
+
+function cargarImagenGatos() {
+    fetch(URL_GATOS)
+    .then(response => response.json())
+    .then(data => {
+        const url = data[0].url
+        $('#img-gatos').src = url
+    })
+}
